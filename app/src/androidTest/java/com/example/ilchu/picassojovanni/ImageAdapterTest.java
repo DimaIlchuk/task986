a50375b17e2e79c435b085e916d2ed9bcabd67af
unknown <ilchukdima04@gmail.com>
2018-07-02 22:21:38 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/ilchu/picassojovanni/ImageAdapterTest.java
@@ -0,0 +1,18 @@
+package com.example.ilchu.picassojovanni;
+
+import org.junit.Test;
+
+public class ImageAdapterTest {
+
+    @Test
+    public void onCreateViewHolder() {
+    }
+
+    @Test
+    public void onBindViewHolder() {
+    }
+
+    @Test
+    public void getItemCount() {
+    }
+}
---
+++ app/src/test/java/com/example/ilchu/picassojovanni/ImageAdapterTest.java
@@ -0,0 +1,5 @@
+package com.example.ilchu.picassojovanni;
+
+public class ImageAdapterTest {
+
+}
